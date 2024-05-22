17e93c62c08dfa228f9a906255858b4f169f6385
mkjni <164661741+mkjni@users.noreply.github.com>
2024-05-22 19:55:37 +0530

    Update myscripts.js

--- myscripts.js
+++ myscripts.js
@@ -8,6 +8,29 @@ function createList(type) {
 
 function generateHTML() {
     const editorContent = document.getElementById('editor').innerHTML;
+    const sanitizedContent = sanitizeHTML(editorContent);
     const output = document.getElementById('output');
-    output.textContent = editorContent;
+    output.textContent = sanitizedContent;
+}
+
+function sanitizeHTML(input) {
+    const tempDiv = document.createElement('div');
+    tempDiv.innerHTML = input;
+
+    const allowedTags = ['P', 'STRONG', 'BR', 'UL', 'OL', 'LI'];
+    const elements = tempDiv.getElementsByTagName('*');
+
+    for (let i = elements.length - 1; i >= 0; i--) {
+        const el = elements[i];
+        if (!allowedTags.includes(el.tagName)) {
+            el.parentNode.removeChild(el);
+        } else {
+            // Remove all attributes
+            for (let j = el.attributes.length - 1; j >= 0; j--) {
+                el.removeAttribute(el.attributes[j].name);
+            }
+        }
+    }
+
+    return tempDiv.innerHTML;
 }
